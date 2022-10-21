0b24bb31623e61c072aab3680e6c95f9924eaffe
Paul Lu <luhanfeng1106@sina.com>
2022-10-21 17:06:21 -0500

    basic working version

--- agent.R
+++ agent.R
@@ -1,3 +1,4 @@
+library(purrr)
 # epsilon greedy action selection
 # returns an array of actions of the agents
 epsilon_greedy_selection <- function(agents, state, epsilon){
@@ -18,31 +19,32 @@ epsilon_greedy_selection <- function(agents, state, epsilon){
 
 # choosing random action based on the probability
 choose_random_action <- function(agents){
-  return (floor(runif(1, min=0, max=dim(agents)[2])));
+  return (sample(1:dim(agents)[2], 1));
 }
 
 # choosing the best action based on the q table
 choose_best_action <- function(agents, agent, state){
   q_table <- c(agents[state,,agent])
-  return (which(q_table==max(q_table)));
+  return (sample(which(q_table==max(q_table)),1));
 }
 
 # updating q table using q learning
-update_q <- function(agents, reward, action, prev_state, state, qparams){
+update_q <- function(agents, reward, actions, prev_state, state, qparams){
   num_of_agents = dim(agents)[3];
   max_q = get_max_q(agents, state); # get the array of max values of the q table
   for(i in 1:num_of_agents){
-    current_q = agents[prev_state,action,i];
-    agents[prev_state,action,i] = current_q + qparams["alpha"] *(reward + qparams["gamma"]*max_q[i] - current_q);
+    current_q = agents[prev_state,actions[i],i];
+    # print(c(typeof(qparams["alpha"]), typeof(qparams["gamma"]), dim(max_q)));
+    agents[prev_state,actions[i],i] <- (current_q + (qparams["alpha"] *(reward + qparams["gamma"]*max_q[1] - current_q)));
   }
+  return(agents)
 }
 
-# get the arraya of max values of the q table
+# get the array of max values of the q table
 get_max_q <- function(agents, state){
   num_of_agents = dim(agents)[3];
-  max_q = replicate(num_of_agents, NULL);
-  for(i in 1:num_of_agents){
-    max_q[i] = max(agents[state,,i]); 
-  }
+  # max_q = replicate(num_of_agents, 1);
+  max_q = map_dbl(1:num_of_agents, function (x) max(agents[state,,x])); 
+  # print(max_q);
   return (max_q);
 }
--- board.R
+++ board.R
@@ -1,20 +1,60 @@
+library(foreach)
 
-
-selectAction <- function(bparams, actions, state) {
+selectAction <- function(bparams, actions, state, rewards) {
   
   # get the current position of the object
-  n <- bparams["width"];
-  m <- bparams["height"];
+  n <- bparams["width"]-bparams["obj_width"] + 1;
+  m <- bparams["height"]-bparams["obj_height"] + 1;
   r <- ((state-1) %/% n)+1;
   column <- (state-1) %% n + 1;
+
   
   
-  if (bparams["cancelation"]) {
+  actions_cnt <- c(0,0,0,0,0);
+  width <- bparams["obj_width"];
+  height <- bparams["obj_height"];
+  for (i in actions) {
     
-  } else {
+    # only record actions of the agents under or next to the object
+    d <- i - state;
+    ri <- ((i-1) %/% n)+1;
+    ci <- (i-1) %% n + 1;
+    d1 <- ri - r;
+    d2 <- ci - column;
+    if (d1 >= 0 && d1 <= height && d2 >= 0 && d2 <= width) {
+      actions_cnt[i] <- actions_cnt[i] + 1
+    } else if ((d1 == -1 || d1 == 1) && d2 >= 0 && d2 <= width) {
+      actions_cnt[i] <- actions_cnt[i] + 3
+    } else if ((d2 == -1 || d2 == 1) && d1 >= 0 && d1 <= height) {
+      actions_cnt[i] <- actions_cnt[i] + 3
+    }
     
   }
   
+  if (bparams["cancelation"]) {
+    actions_cnt[1] <- actions_cnt[1] - actions_cnt[2];
+    actions_cnt[2] <- - actions_cnt[1];
+    actions_cnt[3] <- actions_cnt[3] - actions_cnt[4];
+    actions_cnt[4] <- - actions_cnt[3];
+  }
+  
+  vote <- max(actions_cnt);
+  actions_highest_vote <- c(0,0,0,0,0);
+  cnt <- 0;
+  for (i in 1:5) {
+    if (actions_cnt[i] == vote) {
+      cnt <- cnt + 1;
+      actions_highest_vote[cnt] <- i;
+    }
+  }
+  
+  actions_highest_vote <- actions_highest_vote[1:cnt];
+  if (cnt == 1) {
+    action <- actions_highest_vote[1];
+  } else {
+    action <- sample(actions_highest_vote,1);
+  }
+
   if (action == 1) {
     # move up
     if (r < m) {
@@ -40,8 +80,8 @@ selectAction <- function(bparams, actions, state) {
   state <- column + (r - 1) * n;
   
   # determine the reward
-  
-  rewards <- bparams["rewards"];
+
+
   reward <- rewards[2];
   reached_goal <- FALSE;
   # check if it is at the goal state
